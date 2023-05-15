776ca2a2fd1ade4c11db834097729fc95a7668fc
adylitto <adylitto@gmail.com>
2023-05-15 21:46:29 +0200

    icons

--- components/BuymeaCoffee.jsx
+++ components/BuymeaCoffee.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BuyMeACoffeeButton = () => {
   return (
     <a href="https://www.buymeacoffee.com/adyl"  target="_blank" rel="noopener noreferrer">
-        <img src="https://img.buymeacoffee.com/button-api/?text=M'offrir un café&emoji=☕&slug=adyl&button_colour=BD5FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
+        <img src="https://img.buymeacoffee.com/button-api/?text=M'offrir un café&emoji=☕&slug=adyl&button_colour=BD5FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" alt="m offrir un café" /></a>
   );
 };
 
